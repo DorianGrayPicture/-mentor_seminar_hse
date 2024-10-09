c229411925607b49f98aa94559df609f95ad0feb
Denis <denis123denis2017@yandex.ru>
2024-10-09 18:54:21 +0300

    add text_dev.py

---
+++ text_dev.py
@@ -0,0 +1,10 @@
+class MentorSeminar:
+    def __init__(self) -> None:
+        self.__mentor = "Vladislav"
+
+    def git_push_to_main(self) -> None:
+        """
+        Push to main branch
+        """
+        print("Messing up with repo...")
+
