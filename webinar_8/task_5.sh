3a05a86cababb792673bf960c4ed42bd69471c7a
Denis <denis123denis2017@yandex.ru>
2024-10-18 00:58:17 +0300

    refactor task_5.sh

--- webinar_8/task_5.sh
+++ webinar_8/task_5.sh
@@ -4,10 +4,9 @@ read -p "Enter a directory name: " dir_name
 
 if [ -d "$dir_name" ]; then
 	if [ "$(ls -a $dir_name)" ]; then
-		prefix="backup_"
 		mkdir -p $dir_name/backup
 		for file in $dir_name/*; do
-			echo "Copying..."
+			cp "$file" "$dir_name"/backup/backup_$(basename "$file")
 		done
 	else
 		echo "Directory is empty"
