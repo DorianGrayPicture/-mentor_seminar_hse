bcdac4d705b3cee223fc603cf9c29c05d69e4e71
Denis <denis123denis2017@yandex.ru>
2024-10-18 00:49:20 +0300

    mock up task_5.sh

--- webinar_8/task_5.sh
+++ webinar_8/task_5.sh
@@ -4,9 +4,13 @@ read -p "Enter a directory name: " dir_name
 
 if [ -d "$dir_name" ]; then
 	if [ "$(ls -a $dir_name)" ]; then
-		echo "Directory is not empty"
+		prefix="backup_"
+		mkdir -p $dir_name/backup
+		for file in $dir_name/*; do
+			echo "Copying..."
+		done
 	else
-		echo "Directory is not empty"
+		echo "Directory is empty"
 	fi;
 else
 	echo "Directory does not exist"
