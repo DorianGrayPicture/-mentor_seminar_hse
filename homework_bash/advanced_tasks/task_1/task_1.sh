cfe0682468dd51dfec6102ba8f9765691d3dcf9c
Denis <denis123denis2017@yandex.ru>
2024-11-22 11:26:33 +0300

    task_1 restructured

--- homework_bash/advanced_tasks/task_1/task_1.sh
+++ homework_bash/advanced_tasks/task_1/task_1.sh
@@ -9,15 +9,15 @@ if [[ -d "$DIR_NAME" ]]; then
         # Текущая дата
 	    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
         # Создаем папку, куда будем копировать файлы
-        BACKUP_FOLDER="$DIR_NAME"/backup-"$TIMESTAMP"
+        BACKUP_FOLDER=backup-"$TIMESTAMP"
         mkdir "$BACKUP_FOLDER"
         
         # Копируем файлы из указанной директории в только что созданную папку
         # Переменная count отслеживает количество скопированных файлов
-        # Также в домашней директории создается файл logfile.log, куда записываются логи копирования файлов
+        # Также в создается файл logfile.log, куда записываются логи копирования файлов
         count=0
         for file in "$DIR_NAME"/*; do
-            cp "$file" "$BACKUP_FOLDER" && echo "Файл $file скопирован в $BACKUP_FOLDER" >> ~/logfile.log && ((count++))
+            cp "$file" "$BACKUP_FOLDER" && echo "Файл $file скопирован в $BACKUP_FOLDER" >> logfile.log && ((count++))
         done
         
         # Уведомляем о том, сколько файлов было успешно скопировано
